ad3c4f43b85f137e622d1faa3d5fb1fc9c88ec61
Olga Pankrashina <olyaolya2713@yandex.ru>
2023-06-19 19:39:47 +0300

    add api

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,15 +1,42 @@
+import { useEffect, useState } from 'react';
+import api from '../../utils/api';
+
 function Main({ onEditProfile, onAddPlace, onEditAvatar }) {
+  const [userName, setUserName] = useState('');
+  const [userDescription, setUserDescription] = useState('');
+  const [userAvatar, setUserAvatar] = useState('');
+  //const [card,setCard] = useState('')
+
+  useEffect(() => {
+    // Выполните запрос в API для получения информации о пользователе
+    api
+      .getUserInfo()
+      .then(response => {
+        const { name, about, avatar } = response;
+        setUserName(name);
+        setUserDescription(about);
+        setUserAvatar(avatar);
+      })
+      .catch(error => {
+        console.log('Ошибка при получении информации о пользователе:', error);
+      });
+  }, []);
+
   return (
     <main>
       <section className="content">
         <div className="content__profile">
           <div className="content__avatar">
-            <img src="#" alt="" className="content__photo" />
+            <div
+              style={{ backgroundImage: `url(${userAvatar})` }}
+              alt=""
+              className="content__photo"
+            />
             <div className="content__avatar-overlay" onClick={onEditAvatar} />
           </div>
           <div className="content__edit">
             <div className="content__name">
-              <h1 className="content__title">Hello</h1>
+              <h1 className="content__title">{userName}</h1>
               <button
                 type="button"
                 aria-label="Редактировать"
@@ -17,7 +44,7 @@ function Main({ onEditProfile, onAddPlace, onEditAvatar }) {
                 onClick={onEditProfile}
               />
             </div>
-            <p className="content__subtitle">World</p>
+            <p className="content__subtitle">{userDescription}</p>
           </div>
         </div>
         <button
