5586800cf9e66dac199a9742fae977ceaacf938f
Olga Pankrashina <olyaolya2713@yandex.ru>
2023-07-06 15:08:51 +0300

    add ux

--- src/components/PopupWithForm/PopupWithForm.js
+++ src/components/PopupWithForm/PopupWithForm.js
@@ -1,4 +1,20 @@
+import { useEffect, useState } from 'react';
+
 function PopupWithForm({ name, title, button, children, isPopupOpen, onClose, onSubmit, isValid }) {
+  const [isSaving, setIsSaving] = useState(false);
+
+  const handleFormSubmit = evt => {
+    evt.preventDefault();
+    setIsSaving(true);
+    onSubmit(evt);
+  };
+
+  useEffect(() => {
+    if (!isPopupOpen) {
+      setIsSaving(false);
+    }
+  }, [isPopupOpen]);
+
   return (
     <div
       className={`popup popup_type_${name} ${isPopupOpen ? 'popup_opened' : ''}`}
@@ -6,14 +22,15 @@ function PopupWithForm({ name, title, button, children, isPopupOpen, onClose, on
     >
       <div className="popup__container">
         <h2 className="popup__title">{title}</h2>
-        <form name={name} className="popup__form" noValidate="" onSubmit={onSubmit}>
+        <form name={name} className="popup__form" noValidate="" onSubmit={handleFormSubmit}>
           {children}
           <button
             type="submit"
             aria-label="Сохранить"
-            className={`popup__button ${!isValid ? 'popup__button_disabled' : ''}`}
+            className={`popup__button ${!isValid || isSaving ? 'popup__button_disabled' : ''}`}
+            disabled={!isValid || isSaving}
           >
-            {button}
+            {isSaving ? 'Сохранение...' : button}
           </button>
         </form>
         <button
